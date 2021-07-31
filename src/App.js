77546b9e84b8b226d16cfab0e47c8a13ddfd83d6
RoyLowe <roylowe96@gmail.com>
2021-07-31 12:30:43 -0400

    created monthly specials

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Specials from './components/MonthlySpecials';
 import './App.css';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
                     <NavbarBrand href="/">Rockville Cafe</NavbarBrand>
                 </div>
                 </Navbar>
+                <Specials />
             </div>
         );
     }
---
+++ src/components/MonthlySpecials.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+class Specials extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            specials: [
+                {
+                    id: 0,
+                    name: 'Breakfast',
+                    image: 'assets/images/eggPlateSpecial.jpeg',
+                    description: "2 eggs served with toast spring greens",
+                    cost: "$5.99"
+                },
+                {
+                    id: 1,
+                    name: 'Lunch',
+                    image: 'assets/images/lunchSpecial.jpg',
+                    description: "tuna salad with lettuce, onions, served on wheat bread",
+                    cost: "$8.99"
+                },
+                {
+                    id: 2,
+                    name: 'dinner',
+                    image: 'assets/images/ribDinner.jfif',
+                    description: "prime rib with fries, tomatoes and pickles",
+                    cost: "$19.99"
+                }
+            ]
+        };
+    }
+    render() {
+        const special = this.state.specials.map(monthlySpecial => {
+            return (
+                <div key={monthlySpecial.id}className="col">
+                    <img src={monthlySpecial.image} alt={monthlySpecial.name} />
+                    <h2>{monthlySpecial.name}</h2>
+                    <p>{monthlySpecial.description}</p>
+                    <p>{monthlySpecial.cost}</p>
+                </div>
+            )
+        } );
+
+
+
+        return(
+            <div className="container">
+                <div className="row">
+                    {special}
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Specials;
